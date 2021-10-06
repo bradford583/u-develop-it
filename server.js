72cfecaeaba349d534d16ad5fa579a85e71863c0
Chris Bradford <87334834+bradford583@users.noreply.github.com>
2021-10-05 18:49:58 -0600

    fixed server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json);
+app.use(express.json());
 
 app.get('/', (req, res) => {
+    console.log("hello");
     res.json({
         message: 'Hello World!'
     });
